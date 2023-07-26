978af6b85d6bd6c6d7aaed2ae92a2c6c8c092d4f
Shivam Gupta <gupta.shivam2604@gmail.com>
2023-07-26 21:35:02 +0530

    Started Whatsapp Clone

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './components/ChatListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem />
+      <ChatListItem />
+      <ChatListItem />
+      <ChatListItem />
+      <ChatListItem />
       <StatusBar style="auto" />
     </View>
   );
